9b5e7826d66d45cdfed62e3358d0095ff2d533e5
Jspahiu <121984809+Jspahiu@users.noreply.github.com>
2025-04-30 17:25:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// Index.html
+
+function clicked_description() {
+    alert("Fun Fact: This took months to complete over a period of time!");
+}
+
+function clicked_title() {
+    alert("Hope you liked my title!");
+}
+
+function other_tabs() {
+    window.location.href = "options_tab.html";
+}
+
+// Options_Tab.html
+function home_page_button() {
+    window.location.href = "index.html";
+}
+
+function about_project_button() {
+    window.location.href = "about_project.html";
+}
+
+function canva_slides() {
+    window.location.href = "canva_slides.html";
+}
+
+function project_video_button() {
+    window.location.href = "project_video.html";
+}
+
+function other_tabs_button() {
+    window.location.href = "feedback.html";
+}
